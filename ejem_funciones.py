0a587174feea71f04507fdf4f207e346bc6361c2
JacLealMena <Jac.leal@duocuc.cl>
2024-06-05 10:38:14 -0400

    primer guardado, sin muchos cambios

---
+++ ejem_funciones.py
@@ -0,0 +1,19 @@
+import os
+os.system("cls")
+
+print("""
+            Menú
+
+      1. Sumar 2 números
+      2. Restar 2 números
+      3. Multiplicar 2 números
+      4. Dividir 2 números""")
+opc = int(input("opción:"))
+if opc == 1:
+    pass
+elif opc == 2:
+    pass
+elif opc == 3:
+    pass
+else:
+    pass
